c9f0de4e3f23390449071c158dd50fb892ab4402
javad <j.2528840@gmail.com>
2023-08-17 14:11:12 +0330

    v1

--- web3Transactions.py
+++ web3Transactions.py
@@ -1,8 +1,12 @@
 from web3 import Web3
 from web3.contract import Contract
+from web3.middleware import geth_poa_middleware
+from eth_account import Account
+
 
 providerUrl = 'https://sepolia.infura.io/v3/08bc373c28134da2893b83f543fa3ad3'  
 web3 = Web3(Web3.HTTPProvider(providerUrl))
+web3.middleware_onion.inject(geth_poa_middleware, layer=0)  
 
 print(web3.is_connected())
 
@@ -164,40 +168,31 @@ contractABI = [
 ]
 contract_address = '0xee577210aFA5ECF45B74D65b500324A71ddd6E83'
 contract = web3.eth.contract(address=contract_address, abi=contractABI)
+wallet_address = '0x99baFA590a24755f94F5815df06102C2450AcD3D'
+private_key = ''
 
 # -----------------------------------------------------------------------------------
 
-# VIEW FUNCTIONS
+## VIEW FUNCTIONS
 # index = "0x99baFA590a24755f94F5815df06102C2450AcD3D"  
 # result = contract.functions.getBalance(index).call()
 # print(result)
 
 # -----------------------------------------------------------------------------------
 
-wallet_address = '0x99baFA590a24755f94F5815df06102C2450AcD3D'
-private_key = 'b0f075c393877e332ed691e1341c3897c5b7a28e20861eed200bcdb199c0e745'
-
-from web3.middleware import geth_poa_middleware
-from eth_account import Account
-
-web3.middleware_onion.inject(geth_poa_middleware, layer=0)  # اضافه کردن میان‌افزار برای شبکه POA
-
+# transfer Token functions
 function_address = contract.functions.transfer(100, '0x99baFA590a24755f94F5815df06102C2450AcD3D').address
 
-
-# ایجاد تراکنش
 transaction = {
     'to': contract_address,
     'data': contract.encodeABI(fn_name='transfer', args=[100, '0x99baFA590a24755f94F5815df06102C2450AcD3D']),
-    'gas': 300000,  # مقدار گاز مورد نیاز برای اجرای تابع
-    'gasPrice': web3.to_wei('50', 'gwei'),  # قیمت گاز
-    'nonce': web3.eth.get_transaction_count(wallet_address),  # شماره تراکنش
+    'gas': 200000,  
+    'gasPrice': web3.to_wei('50', 'gwei'),  
+    'nonce': web3.eth.get_transaction_count(wallet_address), 
 }
 
-# امضای تراکنش
 signed_transaction = web3.eth.account.sign_transaction(transaction, private_key=private_key)
 
-# ارسال تراکنش امضا شده به شبکه
 transaction_hash = web3.eth.send_raw_transaction(signed_transaction.rawTransaction)
 
-# پرینت هش تراکنش
+print(f'Transaction sent: {transaction_hash.hex()}')
