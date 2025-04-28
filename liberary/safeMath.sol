004e78ed25f46316e3a08332436d0052c77ad28f
javad <j.2528840@gmail.com>
2025-04-28 09:51:14 +0330

    fix

--- liberary/safeMath.sol
+++ liberary/safeMath.sol
@@ -104,7 +104,7 @@ library SafeMath {
      *
      * - The divisor cannot be zero.
      */
-     
+
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         return div(a, b, "SafeMath: division by zero");
     }
@@ -121,6 +121,7 @@ library SafeMath {
      *
      * - The divisor cannot be zero.
      */
+     
     function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b > 0, errorMessage);
         uint256 c = a / b;
